311cfc57c3c8360d75d53382f84bc436dedc5863
Endry <endrybur@gmail.com>
2022-10-31 22:00:24 +0800

    interruptsT

--- interrupts/interrupts.ino
+++ interrupts/interrupts.ino
@@ -1,19 +1,15 @@
-#define btn_pin 2
-#define led_pin 13
-bool state = LOW;
+#define sensor_pin A4
 
-void setup() {
-  pinMode(btn_pin, INPUT);
-  pinMode(led_pin, OUTPUT);
-  int int_number = digitalPinToInterrupt(btn_pin);
-  attachInterrupt(int_number, _blink,  RISING);
-  Serial.begin(9600);
+void setup()
+{
+    pinMode(sensor_pin, INPUT);
+    Serial.begin(9600);
 }
 
-void loop() {
-  digitalWrite(led_pin, state);
-}
-
-void _blink(){
-  state=!state;
+void loop()
+{
+    char str = int(analogRead(sensor_pin) / 10.24);
+//    Serial.write(val);
+    Serial.write(str);
+    delay(1000);
 }
